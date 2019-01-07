8ad784e21075a1cd331bcebeef76021505f1a595
Andre <fendred@yahoo.com>
2019-01-07 12:48:46 -0500

    Create User Model

---
+++ app/models/user.rb
@@ -0,0 +1,54 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  username        :string           not null
+#  email           :string           not null
+#  image_url       :string           default("default_avatar.png")
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :email, :password_digest, :session_token, presence: true
+  validates :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 8, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  has_many :cities, foreign_key: :user_id, class_name: 'City', dependent: :destroy
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return user if user && user.is_password?(password)
+  end
+
+  def generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = generate_session_token
+    self.save!
+    self.session_token
+  end
+  
+  private
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+end
