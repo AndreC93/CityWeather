0f207f00a8d4f8305967282b23e0fdfc86a2b355
Andre <fendred@yahoo.com>
2019-01-15 11:17:32 -0500

    Implement Footer component with personal links

---
+++ frontend/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div id="footer">
+      <a href="https://github.com/AndreC93" target="_blank">
+        <img src="/assets/github.png" alt="Github" />
+      </a>
+      <a href="https://www.linkedin.com/in/andre-chow/" target="_blank">
+        <img src="/assets/linkedIn.png" alt="LinkedIn" />
+      </a>
+      <a href="https://www.andrechow.com/" target="_blank">
+        <img src="/assets/ac_logo.png" alt="Visit my Site" />
+      </a>
+    </div>
+  )
+}
+
