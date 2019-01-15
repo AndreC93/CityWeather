657298e7e1d752d3db666d21510eedf5de9331b2
Andre <fendred@yahoo.com>
2019-01-15 12:55:09 -0500

    Fix src for Personal links

--- frontend/components/Footer.jsx
+++ frontend/components/Footer.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 
 export default function Footer() {
+  const { github, linkedIn, acLogo } = window;
+
   return (
     <div id="footer">
       <a href="https://github.com/AndreC93" target="_blank">
-        <img src="/assets/github.png" alt="Github" />
+        <img src={github} alt="Github" />
       </a>
       <a href="https://www.linkedin.com/in/andre-chow/" target="_blank">
-        <img src="/assets/linkedIn.png" alt="LinkedIn" />
+        <img src={linkedIn} alt="LinkedIn" />
       </a>
       <a href="https://www.andrechow.com/" target="_blank">
-        <img src="/assets/ac_logo.png" alt="Visit my Site" />
+        <img src={acLogo} alt="Visit my Site" />
       </a>
     </div>
   )
